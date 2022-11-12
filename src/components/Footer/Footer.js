df426326b9e582e8d1bd4ef23e5a837d8b5f91ee
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-12 23:08:35 +0100

    Built Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,45 @@ import React from "react";
 
 export default function Footer () {
     return (
-        <></>
+        <div className="bg-[#1D1D1E] px-28 pt-14 text-[#F1F3F9] text-left">
+          <div className="flex justify-between">
+            <div>
+                <img src="/images/footer-logo.svg" alt="Meta-bnb"></img>
+                <div className="flex justify-between pt-24 w-32">
+                  <img src="/images/facebook.png" alt="facebook"></img>
+                  <img src="/images/instagram.png" alt="instagram"></img>
+                  <img src="/images/twitter.png" alt="twitter"></img>
+                </div>
+            </div>
+            <div>
+                <p className="font-bold text-lg pb-4">Community</p>
+                <ul>
+                    <li className="text-sm leading-9">NFT</li>
+                    <li className="text-sm leading-9">Tokens</li>
+                    <li className="text-sm leading-9">Landlords</li>
+                    <li className="text-sm leading-9">Discord</li>
+                </ul>
+            </div>
+            <div>
+                <p className="font-bold text-lg pb-4">Places</p>
+                <ul>
+                    <li className="text-sm leading-9">Castle</li>
+                    <li className="text-sm leading-9">Farms</li>
+                    <li className="text-sm leading-9">Beach</li>
+                    <li className="text-sm leading-9">Learn more</li>
+                </ul>
+            </div>
+            <div>
+                <p className="font-bold text-lg pb-4">About us</p>
+                <ul>
+                    <li className="text-sm leading-9">Road map</li>
+                    <li className="text-sm leading-9">Creators</li>
+                    <li className="text-sm leading-9">Career</li>
+                    <li className="text-sm leading-9">Contact us</li>
+                </ul>
+            </div>
+            </div>
+            <p className="py-9">&copy; 2022 Metabnb</p>
+        </div>
     )
 }
