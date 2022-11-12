0eca11b6221ddd0b31597f0daeccc3ad86246f14
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-12 17:49:50 +0100

    Rendered Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
+import Head from './components/Head/Head';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Head />
     </div>
   );
 }
