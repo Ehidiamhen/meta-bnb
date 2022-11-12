a2589766c3d5e2d2d91fac21e1d29c8111da88c9
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-12 21:10:01 +0100

    Rendered Location component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import Head from './components/Head/Head';
+import Locations from './components/Locations/Locations';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Head />
+      <Locations />
     </div>
   );
 }
