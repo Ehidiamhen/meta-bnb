9206c9bf292ef9e5ee68a02b96e712371afe1eae
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-12 17:42:54 +0100

    Added an extended color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#A02279"
+      }
+    },
   },
   plugins: [],
 }
