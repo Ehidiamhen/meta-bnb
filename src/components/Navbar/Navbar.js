1859aed65ed63810a34c092d87aa7cf7c3a81a0e
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-13 23:14:22 +0100

    Implemented necessary changes for routing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar () {
     return (
         <nav className='bg-white border-gray-200 px-2 sm:px-16 py-2.5 rounded dark:bg-gray-900'>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <a href='#' className='flex items-center'>
+                <a href='/' className='flex items-center'>
                     <img src={'/images/logo.png'} className='h-6 sm:h-9'alt='Meta-bnb'></img>
                 </a>
                 <div className='flex md:order-2'>
@@ -19,16 +19,16 @@ export default function Navbar () {
                 <div className='hidden justify-between items-center w-full md:flex md:w-auto md:order-1' id='navbar-cta'>
                     <ul className='flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-xl md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-[purple] rounded md:bg-transparent md:p-0" aria-current="page">Home</a>
+                      <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-[purple] rounded md:bg-transparent md:p-0" aria-current="page">Home</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[purple] md:p-0">Place to stay</a>
+                      <a href="/places" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[purple] md:p-0">Place to stay</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</a>
+                      <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">Community</a>
+                      <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">Community</a>
                     </li>
                     </ul>
                 </div>
