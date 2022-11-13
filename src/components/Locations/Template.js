e45eeb2195d3d6db2f2eeee765be4a2fe797c96b
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-13 19:18:46 +0100

    Completed template structure

--- src/components/Locations/Template.js
+++ src/components/Locations/Template.js
@@ -6,17 +6,17 @@ export default function Place (props) {
         alt,
     } = props
     return (
-        <div className="border">
+        <div className="border rounded-2xl w-72 px-4 mr-6 py-4 mb-6 text-xs text-[#434343]">
             <img src={src} alt={alt}></img>
-            <div>
-              <p>Desert king</p>
-              <p>1MBT per night</p>
+            <div className="flex justify-between pt-4 py-2">
+              <p className="">Desert king</p>
+              <p className="font-bold">1MBT per night</p>
             </div>
-            <div>
-              <p>2345km away</p>
-              <p>available for 2weeks stay</p>
+            <div className="flex justify-between">
+              <p className="pb-2">2345km away</p>
+              <p className="">available for 2weeks stay</p>
             </div>
-            <img src="" alt=""></img>
+            <img src={'/images/locations/rating.png'} alt=""></img>
         </div>
     )
 }
