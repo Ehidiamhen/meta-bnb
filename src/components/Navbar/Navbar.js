8736ef474dfe561106c25c5c8516b8844e9f78c1
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-13 23:34:57 +0100

    Implemented Link component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from '../Link/Link'
 
 export default function Navbar () {
     return (
         <nav className='bg-white border-gray-200 px-2 sm:px-16 py-2.5 rounded dark:bg-gray-900'>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <a href='/' className='flex items-center'>
+                <Link href='/' className='flex items-center'>
                     <img src={'/images/logo.png'} className='h-6 sm:h-9'alt='Meta-bnb'></img>
-                </a>
+                </Link>
                 <div className='flex md:order-2'>
                     <button type='button' className='text-white bg-purple hover:bg-[purple] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0'>
                         Connect Wallet
@@ -19,16 +20,16 @@ export default function Navbar () {
                 <div className='hidden justify-between items-center w-full md:flex md:w-auto md:order-1' id='navbar-cta'>
                     <ul className='flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-xl md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li>
-                      <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-[purple] rounded md:bg-transparent md:p-0" aria-current="page">Home</a>
+                      <Link href="/" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-[purple] rounded md:bg-transparent md:p-0" aria-current="page">Home</Link>
                     </li>
                     <li>
-                      <a href="/places" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[purple] md:p-0">Place to stay</a>
+                      <Link href="/places" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[purple] md:p-0">Place to stay</Link>
                     </li>
                     <li>
-                      <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</a>
+                      <Link href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</Link>
                     </li>
                     <li>
-                      <a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">Community</a>
+                      <Link href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">Community</Link>
                     </li>
                     </ul>
                 </div>
