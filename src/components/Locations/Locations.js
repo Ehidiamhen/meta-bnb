736c50f48ea9293fea6a2c1183fe1b6047d04e99
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-13 17:32:03 +0100

    Rendered component for locations

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Place from "./Template";
 
 export default function Locations () {
     return (
-        <p className="font-bold text-5xl py-14">Inspiration for your next adventure</p>
+        <>
+            <p className="font-bold text-5xl py-14">Inspiration for your next adventure</p>
+            <Place />
+        </>
     )
 }
