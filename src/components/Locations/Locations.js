d1faab221fb08090c52916afa22eff4f70c386ea
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-13 19:20:04 +0100

    Completed the rendering of locations using the template component

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -5,7 +5,36 @@ export default function Locations () {
     return (
         <>
             <p className="font-bold text-5xl py-14">Inspiration for your next adventure</p>
-            <Place />
+            <div className="ml-16">
+                <div className="flex">
+                    <Place 
+                      src={'/images/locations/place-5.png'}
+                    />
+                    <Place 
+                      src={'/images/locations/place-6.png'}
+                    />
+                    <Place 
+                      src={'/images/locations/place-7.png'}
+                    />
+                    <Place 
+                      src={'/images/locations/place-8.png'}
+                    />
+                </div>
+                <div className="flex mb-7">
+                    <Place 
+                      src={'/images/locations/place-1.png'}
+                    />
+                    <Place 
+                      src={'/images/locations/place-2.png'}
+                    />
+                    <Place 
+                      src={'/images/locations/place-3.png'}
+                    />
+                    <Place 
+                      src={'/images/locations/place-4.png'}
+                    />
+                </div>
+            </div>
         </>
     )
 }
