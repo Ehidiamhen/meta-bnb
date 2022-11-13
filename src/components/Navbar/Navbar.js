6406b20b2249d4d7dbb2bbcb6a7ec1417c298115
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-13 19:16:16 +0100

    Updated font size and colors

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar () {
                     <img src={'/images/logo.png'} className='h-6 sm:h-9'alt='Meta-bnb'></img>
                 </a>
                 <div className='flex md:order-2'>
-                    <button type='button' className='text-white bg-[purple] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                    <button type='button' className='text-white bg-purple hover:bg-[purple] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0'>
                         Connect Wallet
                     </button>
                     <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 text-sm text-gray-500  rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
@@ -17,18 +17,18 @@ export default function Navbar () {
                     </button>
                 </div>
                 <div className='hidden justify-between items-center w-full md:flex md:w-auto md:order-1' id='navbar-cta'>
-                    <ul className='flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                    <ul className='flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-xl md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 hover:text-[purple] rounded md:bg-transparent md:p-0" aria-current="page">Home</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Place to stay</a>
+                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[purple] md:p-0">Place to stay</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</a>
+                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</a>
                     </li>
                     <li>
-                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Community</a>
+                      <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">Community</a>
                     </li>
                     </ul>
                 </div>
