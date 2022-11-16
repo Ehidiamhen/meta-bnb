7039d7c911ebdf7c8873aa8af99dbc987fd76339
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-11-16 17:40:11 +0100

    Fixed bugs

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar () {
                       <Link href="/places" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[purple] md:p-0">Place to stay</Link>
                     </li>
                     <li>
-                      <Link href="/" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">NFTs</Link>
+                      <Link href="/" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">NFTs</Link>
                     </li>
                     <li>
                       <Link href="/" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-[purple] md:hover:bg-transparent md:hover:text-[purple] md:p-0">Community</Link>
